16d69eb0cd6fead9f7b07b46f0dbce3b72ddcb2c
YJ Park <yjpark@gmail.com>
2025-02-25 23:50:29 +0800

    wip

--- crates/util/src/misc/text.rs
+++ crates/util/src/misc/text.rs
@@ -13,20 +13,21 @@ pub fn spawn(
     y: f32,
     z: f32,
 ) -> Entity {
-    let style = TextStyle {
+    let text_font = TextFont {
         font,
         font_size,
-        color,
     };
     let text_entity = commands
-        .spawn(Text2dBundle {
-            text: Text::from_section(text, style).with_justify(justify),
-            transform: Transform::from_xyz(x, y, z),
-            text_anchor: anchor,
-            ..Default::default()
-        })
+        .spawn((
+            Text2d::new(text),
+            TextLayout::new_with_justify(justify),
+            text_font,
+            TextColor::from(color),
+            Transform::from_xyz(x, y, z),
+            anchor,
+        ))
         .id();
-    commands.entity(entity).push_children(&[text_entity]);
+    commands.entity(entity).add_children(&[text_entity]);
     text_entity
 }
 pub fn set_size(text: &mut Text, font_size: f32) {
